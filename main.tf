97c4a3e60e690d71d5de3005b80497ec64566dfd
Paulo Renato Licciardi <paulo.renato@accurate.com.br>
2023-12-15 13:59:55 -0300

    nova versao terraform

--- main.tf
+++ main.tf
@@ -4,19 +4,96 @@
 #}
 
 
-terraform {
-  backend "s3" {
-    bucket = "prpdbl-bucket1"
-    key = "terraform/backend"
-    region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIAUESVT2CQBEDBBVBX"
+  secret_key = "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"
+
+}
+
+resource "aws_security_group" "project-iac-sg" {
+  name = "IAC-Sec-Group"
+  description = "IAC-Sec-Group"
+  vpc_id = "vpc-03226c7780672b16d"
+
+  // To Allow SSH Transport
+  ingress {
+    from_port = 22
+    protocol = "tcp"
+    to_port = 22
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  // To Allow Port 80 Transport
+  ingress {
+    from_port = 80
+#    protocol = ""
+    protocol        = "TCP"
+    to_port = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+#    protocol        = "-1"
+    protocol        = "TCP"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
-locals {
-  env_name = "sandbox"
-  aws_region = "us-east-1"
-  k8s_cluster_name = "ms-cluster"
+
+resource "aws_instance" "project-iac" {
+#  ami = "ami-0c1bea58988a989155"
+  ami = "ami-0759f51a90924c166"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0d70178f51032d9ac" #FFXsubnet2
+  associate_public_ip_address = true
+  key_name = "myseckey"
+
+
+  vpc_security_group_ids = [
+    aws_security_group.project-iac-sg.id
+  ]
+  root_block_device {
+    delete_on_termination = true
+    iops = 150
+    volume_size = 50
+    volume_type = "gp3"
+  }
+  tags = {
+    Name ="SERVER01"
+    Environment = "DEV"
+    OS = "UBUNTU"
+    Managed = "IAC"
+  }
+
+  depends_on = [ aws_security_group.project-iac-sg ]
 }
+
+
+output "ec2instance" {
+  value = aws_instance.project-iac.public_ip
+}
+
+
+#terraform {
+#  backend "s3" {
+#    bucket = "prpdbl-bucket1"
+#    key = "terraform/backend"
+#    region = "us-east-1"
+#  }
+#}
+
+#locals {
+#  env_name = "sandbox"
+#  aws_region = "us-east-1"
+#  k8s_cluster_name = "ms-cluster"
+#}
 
 # Network Configuration ... 
 
