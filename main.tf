1a421df13c7de49bbb6055d7db1a45b5857b7cc4
Paulo Renato Licciardi <paulo.renato@accurate.com.br>
2023-12-26 15:07:40 -0300

    nova tentativa

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ variable "AWS_SECRET_ACCESS_KEY" {
 
 provider "aws" {
   region = "us-east-1"
-  access_key = "${var.AWS_ACCESS_KEY_ID}"
-  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
+#  access_key = "${var.AWS_ACCESS_KEY_ID}"
+#  secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
 }
 
 resource "aws_security_group" "project-iac-sg" {
